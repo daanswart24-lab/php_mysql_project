a38b31dd6fe4da20b6d7c94785a97a6bb153dac2
Daan <daanswart24@gmail.com>
2026-07-03 15:12:42 +0200

    Add first PHP page with basic HTML structure

---
+++ 01-05_Basics/h01/first.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP Page</title>
+</head>
+<body>
+    <!-- Script 1.2 - first.php -->
+     <p>This is standard HTML.</p>
+     
+     <?php
+     ?>
+</body>
+</html>
