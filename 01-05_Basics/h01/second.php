bab4d6ed88f10e0d60af29f26452fa7ecb30bdfa
Daan <daanswart24@gmail.com>
2026-07-05 15:22:42 +0200

    Add second PHP page demonstrating HTML and PHP integration

---
+++ 01-05_Basics/h01/second.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using Echo</title>
+</head>
+<body>
+    <!-- Script 1.3 - second.php -->
+     <p>This is standard HTML.</p>
+     
+     <?php
+     echo "<p>This was generated using PHP.</p>";
+     ?>
+</body>
+</html>
