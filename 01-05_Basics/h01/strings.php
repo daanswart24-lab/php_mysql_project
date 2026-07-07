7d8b602e0bb3fd4c7607aa9ac0dc95c18248bc00
Daan <daanswart24@gmail.com>
2026-07-07 20:39:52 +0200

    Add strings.php file demonstrating the use of strings in PHP

---
+++ 01-05_Basics/h01/strings.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    <?php # Script 1.6 - strings.php
+
+    // This script demonstrates the use of strings.
+
+    // Create some variables:
+    $first_name = 'Haruki';
+    $last_name = 'Murakami';
+    $book = 'Kafka on the Shore';
+
+    // Print a message:
+    echo "<p>The book <em>$book</em> was written by $first_name $last_name.</p>";
+    
+    ?>
