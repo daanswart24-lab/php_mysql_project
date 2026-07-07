093968c9624c7e56dd32156c02445ceaf1ccd7d9
Daan <daanswart24@gmail.com>
2026-07-07 16:12:02 +0200

    Add comments.php file with basic HTML structure and PHP script

---
+++ 01-05_Basics/h01/comments.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments</title>
+</head>
+<body>
+    <?php
+
+    # Script 1.4 - comments.php
+    # Created July 7, 2026
+    # Created by Daan Swart
+    # This script does nothing much
+
+    echo '<p>This is a line of text.<br> This another line of text.</p>';
+
+    /*
+    echo '<p>This line of text will not be executed</p>';
+    */
+
+    echo  "<p>Now I'm done.</p>";
+    // End of PHP code.
+
+    ?>
+</body>
+</html>
