f1c62fcdd9420196879351c1c462fbc48bb487f6
Daan <daanswart24@gmail.com>
2026-09-03 14:38:53 +0200

    Refactor calendar.php to streamline drop-down menu generation for day and year

--- 01-05_Basics/h02/calendar.php
+++ 01-05_Basics/h02/calendar.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
+    <style>
+        select{
+            margin-right: 2px;
+        }
+    </style>
 </head>
+
 <body>
     <?php # Script 2.6 - calendar.php 
 
@@ -13,13 +20,7 @@
     // Create the months array:
     $months = array(1 => 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
 
-    // Create the day arrays:
-    $day = range(1, 31);
-
-    // Create the year array:
-    $year = range(2016, 2026);
-
-     // Make the month drop-down menu:
+    // Make the month drop-down menu:
     echo '<select name="month">';
     foreach ($months as $key => $value) {
         echo "<option value=\"$key\">$value</option>";
@@ -28,15 +29,16 @@
 
     // Make the day drop-down menu:
     echo '<select name="day">';
-    foreach ($day as $value) {
-        echo "<option value=\"$value\">$value</option>";
+    for ($day = 1; $day <= 31; $day++) {
+        echo "<option value=\"$day\">$day</option>\n";
     }
     echo '</select>';
-    
+
     // Make the year drop-down menu:
     echo '<select name="year">';
-    foreach ($year as $value) {
-        echo "<option value=\"$value\">$value</option>";
+    for ($year = 2016; $year <= 2026; $year++) {
+        echo "<option value=\"$year\">$year</option>\n";
     }
     echo '</select>';
+
     ?>
